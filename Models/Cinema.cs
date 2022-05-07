c974accfda0eb5a5e4bd782138f7ec74da5bac33
richie48 <57833496+richie48@users.noreply.github.com>
2022-05-07 19:18:31 +0100

    made changes to the models and added new views

--- Models/Cinema.cs
+++ Models/Cinema.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using new_testapp.Data.Base;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace new_testapp.Models
 {
-    public class Cinema
+    public class Cinema:IEntityBase
     {
         [Key]
         public int Id { get; set; }
--- Models/Producer.cs
+++ Models/Producer.cs
@@ -1,23 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using new_testapp.Data.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace new_testapp.Models
 {
-    public class Producer
+    public class Producer:IEntityBase
     {
         [Key]
         public int Id { get; set; }
 
 
         [Display(Name ="Profile Picture")]
+        [Required(ErrorMessage ="Profile picture required")]
         public string ProfilePictureURL { get; set; }
 
 
         [Display(Name ="Full Name")]
+        [Required(ErrorMessage = "full name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Fullname should be between 3 and 50 characters")]
         public string FullName { get; set; }
 
 
 
         [Display(Name ="Biography")]
+        [Required(ErrorMessage = "Bio is required")]
         public string Bio { get; set; }
     }
 }
